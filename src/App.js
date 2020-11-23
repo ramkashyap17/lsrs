39386a405cfabc39b7afe5aef0e7ff8ddc630f58
Ram Kashyap <ramkashyap17@gmail.com>
2020-11-23 15:53:02 +0530

    Solution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var arr = [3, 1, 4, 2, 9]
+  var diff = []
+
+  for(var i = 0; i < arr.length; i++){
+    var input = arr[i]
+    var inputIndex = i
+    var ls = 0
+    var rs = 0
+
+    for(var j = 0; j <= i; j++){
+      ls += arr[j]
+    }
+
+    for(var j = i; j < arr.length; j++){
+      rs += arr[j]
+    }
+
+    diff.push(rs - ls)
+  }
+
+  var min = diff[0]
+  
+  for(var i = 0; i < diff.length; i++){
+    if(diff[i] < min){
+      min = diff[i]
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {"This is the result: " + min}
         </p>
         <a
           className="App-link"
@@ -23,3 +51,4 @@ function App() {
 }
 
 export default App;
+
